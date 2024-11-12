68803d7ea027629c5d55168d7c1a9fbb8a4782d2
vinodh1988 <k.c.vinodh@hotmail.com>
2024-11-12 15:15:03 +0530

    controller-change

--- src/main/java/com/fullapp/controllers/MVCController.java
+++ src/main/java/com/fullapp/controllers/MVCController.java
@@ -1,13 +1,20 @@
 package com.fullapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fullapp.services.DataService;
 
 @Controller
 @RequestMapping("/web")
 public class MVCController {
+	@Autowired
+	private DataService dservice;
    @RequestMapping("/home")
-   public String doIt() {
+   public String doIt(ModelMap map) {
+	   map.addAttribute("names",dservice.getNames());
 	   return "home";
    }
 }
